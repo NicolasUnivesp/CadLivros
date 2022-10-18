774d364d5313987c02ae753015d4684323be05a8
Nicolas de Queiroz Cipriano <nicolas.q.c@hotmail.com>
2022-10-18 07:13:17 -0300

    autenticação rondando limpo

--- livro/urls.py
+++ livro/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from . views import ListaLivroView, LivroCreateView, LivroUpdateView, LivroDeleteView
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     # classes:
-    path('', ListaLivroView.as_view(), name='livro.index'),
-    path('novo/', LivroCreateView.as_view(), name='livro.novo'),
-    path('<int:pk>/editar', LivroUpdateView.as_view(), name='livro.editar'),
-    path('<int:pk>/remover', LivroDeleteView.as_view(), name='livro.remover'),
+    path('', login_required(ListaLivroView.as_view()), name='livro.index'),
+    path('novo/', login_required(LivroCreateView.as_view()), name='livro.novo'),
+    path('<int:pk>/editar', login_required(LivroUpdateView.as_view()), name='livro.editar'),
+    path('<int:pk>/remover', login_required(LivroDeleteView.as_view()), name='livro.remover'),
     
     
     # metodos :
-    path('<int:pk_livro>/emprestimo', views.emprestimo, name='livro.emprestimo'),
-    path('<int:pk_livro>/emprestimo/novo', views.emprestimo_novo, name='emprestimo.novo'),
-    path('<int:pk_livro>/emprestimo/<int:pk>/editar', views.emprestimo_editar, name='emprestimo.editar'),
-    path('<int:pk_livro>/emprestimo/<int:pk>/remover', views.emprestimo_remover, name='emprestimo.remover'),
+    path('<int:pk_livro>/emprestimo', login_required(views.emprestimo), name='livro.emprestimo'),
+    path('<int:pk_livro>/emprestimo/novo', login_required(views.emprestimo_novo), name='emprestimo.novo'),
+    path('<int:pk_livro>/emprestimo/<int:pk>/editar', login_required(views.emprestimo_editar), name='emprestimo.editar'),
+    path('<int:pk_livro>/emprestimo/<int:pk>/remover', login_required(views.emprestimo_remover), name='emprestimo.remover'),
     
 ]
--- project/urls.py
+++ project/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('livros/', include('livro.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+    
 ]
