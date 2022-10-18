f81ce42d9e5ae19e45c982508726ba1182eb35a9
Nicolas de Queiroz Cipriano <nicolas.q.c@hotmail.com>
2022-10-18 09:16:08 -0300

    cadastro de usuarios

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django import views
 from django.urls import path
 from .views import HomeView
-# from .views import register
+from .views import register
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('', login_required(HomeView.as_view()), name='home'),
-    # path('accounts/register', register, name='register')
+    path('accounts/register', register, name='register')
 ]
